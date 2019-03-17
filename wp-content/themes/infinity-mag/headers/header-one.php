6ee22e0d68d71aa05a488694eb9117ca70616a03
hossamhilal <46751584+hossamhilal@users.noreply.github.com>
2019-03-17 16:35:54 +0200

    navbar

--- wp-content/themes/infinity-mag/headers/header-one.php
+++ wp-content/themes/infinity-mag/headers/header-one.php
@@ -1,15 +1,40 @@
-<nav class="dw_navigation" id="dw_navigation">
-	<?php
-	wp_nav_menu(
-		array(
-			'theme_location' => 'header_menu',
-			'depth'          => 3,
-			'container'      => false,
-			'menu_class'     => 'menu',
-			'fallback_cb'    => 'window_mag_nav_fallback'
-		)
-	);
+
+<div class="dw-navbar">
+    <div class="top-nav">
+        <div class="logo-box">
+            <img src="" alt="img" class="img-fluid">
+        </div>
+        <div class="dw-menu">
+            <nav class="dw_navigation" id="dw_navigation">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'header_menu',
+                        'depth'          => 3,
+                        'container'      => false,
+                        'menu_class'     => 'menu',
+                        'fallback_cb'    => 'window_mag_nav_fallback'
+                    )
+                );
+                ?>
+            </nav>
+        </div>
+        <div class="dw-links">
+            <ul>
+                <li> <a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i> </a> </li>
+                <li> <a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i> </a> </li>
+                <li> <a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i> </a> </li>
+            </ul>
+            <div class="dw-art"> <i class="fa fa-shopping-cart" aria-hidden="true"></i> </div>
+            <div class="dw-search-btn"> <i class="fa fa-search" aria-hidden="true"></i> </div>
+        </div>
+    </div>
+    <div class="wd-nav-banner">
+         <div class="banner-box">
+             <img src="" alt="img" class="img-fluid">
+         </div>
+    </div>
+</div>
 
 
-	?>
-</nav>
+
