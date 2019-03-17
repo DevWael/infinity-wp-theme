b1324730edf981d8a3d76356fad8201244d7cbdd
Ahmad Wael <dev.ahmedwael@gmail.com>
2019-03-17 11:35:02 +0200

    added navbar

--- wp-content/themes/infinity-mag/headers/header-one.php
+++ wp-content/themes/infinity-mag/headers/header-one.php
@@ -1,1 +1,13 @@
-11111
+<nav id="dw_navigation">
+	<?php
+	wp_nav_menu(
+		array(
+			'theme_location' => 'header_menu',
+			'depth'          => 3,
+			'container'      => false,
+			'menu_class'     => 'menu',
+			'fallback_cb'    => 'window_mag_nav_fallback'
+		)
+	);
+	?>
+</nav>
