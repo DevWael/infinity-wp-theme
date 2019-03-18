92912a88a55156607dfdcc3a94fc1013ea4a2037
hossamhilal <46751584+hossamhilal@users.noreply.github.com>
2019-03-18 09:49:37 +0200

    navbar banner

--- wp-content/themes/infinity-mag/headers/header-one.php
+++ wp-content/themes/infinity-mag/headers/header-one.php
@@ -1,25 +1,26 @@
 
 <div class="dw-navbar">
     <div class="top-nav">
-        <div class="logo-box">
-            <img src="<?php get_template_directory_uri(); ?> /images/logo.png" alt="img" class="img-fluid">
-        </div>
-        <div class="dw-menu">
-            <nav class="dw_navigation" id="dw_navigation">
-                <?php
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'header_menu',
-                        'depth'          => 3,
-                        'container'      => false,
-                        'menu_class'     => 'menu',
-                        'fallback_cb'    => 'window_mag_nav_fallback'
-                    )
-                );
-                ?>
-            </nav>
-        </div>
-        <div class="dw-links">
+        <div class="container">
+            <a class="logo-box">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ui/logo.png" alt="img" class="img-fluid">
+            </a>
+            <div class="dw-menu">
+                <nav class="dw_navigation" id="dw_navigation">
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'header_menu',
+                            'depth'          => 3,
+                            'container'      => false,
+                            'menu_class'     => 'menu',
+                            'fallback_cb'    => 'window_mag_nav_fallback'
+                        )
+                    );
+                    ?>
+                </nav>
+            </div>
+            <div class="dw-links">
             <ul class="dw-nav-social">
                 <li> <a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i> </a> </li>
                 <li> <a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i> </a> </li>
@@ -28,11 +29,14 @@
             <div class="dw-art"> <i class="fa fa-shopping-cart" aria-hidden="true"></i> </div>
             <div class="dw-search-btn"> <i class="fa fa-search" aria-hidden="true"></i> </div>
         </div>
+        </div>
     </div>
     <div class="wd-nav-banner">
-         <div class="banner-box">
-             <img src="" alt="img" class="img-fluid">
-         </div>
+        <div class="container">
+             <a class="banner-box">
+                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ui/banner.png" alt="img" class="img-fluid">
+             </a>
+        </div>
     </div>
 </div>
 
