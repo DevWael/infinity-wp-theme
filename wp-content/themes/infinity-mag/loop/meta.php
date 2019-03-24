f5887cdc1e4b589f4c8edcaa3c535c9261e59f01
Ahmad Wael <dev.ahmedwael@gmail.com>
2019-03-24 09:46:08 +0200

    css fixes

--- wp-content/themes/infinity-mag/loop/meta.php
+++ wp-content/themes/infinity-mag/loop/meta.php
@@ -2,12 +2,12 @@
     <ul>
         <li>
             <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ui/pen.svg" alt="icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ui/pen.png" alt="icon">
                 <span> <?php echo get_the_author_meta( 'display_name' ) ?> </span>
             </a>
         </li>
         <li>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ui/calender.svg"
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ui/calender.png"
                  alt="icon">
             <span>
                 <a href="<?php the_permalink() ?>">
@@ -16,7 +16,7 @@
             </span>
         </li>
         <li>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ui/chat.svg"
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ui/chat.png"
                  alt="icon"> <span>
                 <a href="<?php comments_link() ?>">
                     <?php comments_number() ?>
