67ecfd5af98016eeb4043cffeca0f58a3ff3c38a
Ahmad Wael <dev.ahmedwael@gmail.com>
2019-04-01 11:25:24 +0200

    added new options to home page sections changed default sidebar name

--- wp-content/themes/infinity-mag/framework-customizations/theme/options/home.php
+++ wp-content/themes/infinity-mag/framework-customizations/theme/options/home.php
@@ -8,11 +8,25 @@ $options = array(
 		'title'   => esc_html__( 'Home', 'dw' ),
 		'type'    => 'tab',
 		'options' => array(
-			'full_width_1'      => array(
+			'full_width_1'   => array(
 				'title'   => esc_html__( 'Full Section', 'dw' ),
 				'type'    => 'tab',
 				'options' => array(
-					'home_full_width_1' => array(
+					'home_full_enable_1'   => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Activate', 'dw' ),
+						'desc'         => esc_html__( 'Show/Hide This section', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Yes', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hide',
+							'label' => esc_html__( 'No', 'dw' )
+						),
+					),
+					'home_full_width_1'    => array(
 						'type'            => 'addable-popup',
 						'label'           => esc_html__( 'Home Full Width Area 1', 'dw' ),
 						'template'        => '{{- block_title }}',
@@ -29,13 +43,53 @@ $options = array(
 							'layout_type' => dw_full_width_area()
 						)
 					),
+					'home_full_computer_1' => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Show on computer devices', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Show', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hidden-md visible-xs',
+							'label' => esc_html__( 'Hide', 'dw' )
+						),
+					),
+					'home_full_mobile_1'   => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Show on mobile devices', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Show', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hidden-xs',
+							'label' => esc_html__( 'Hide', 'dw' )
+						),
+					),
 				)
 			),
-			'half_section_1'  => array(
+			'half_section_1' => array(
 				'title'   => esc_html__( 'Half Section', 'dw' ),
 				'type'    => 'tab',
 				'options' => array(
-					'home_half_width_1' => array(
+					'home_half_enable_1'   => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Activate', 'dw' ),
+						'desc'         => esc_html__( 'Show/Hide This section', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Yes', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hide',
+							'label' => esc_html__( 'No', 'dw' )
+						),
+					),
+					'home_half_width_1'    => array(
 						'type'            => 'addable-popup',
 						'label'           => esc_html__( 'Home Half Width Area 1', 'dw' ),
 						'template'        => '{{- block_title }}',
@@ -52,7 +106,7 @@ $options = array(
 							'layout_type' => dw_half_width_area()
 						)
 					),
-					'sidebar_area_1'      => array(
+					'sidebar_area_1'       => array(
 						'type'         => 'switch',
 						'value'        => 'right_sidebar',
 						'label'        => esc_html__( 'Area (1) Sidebar Position', 'dw' ),
@@ -65,13 +119,53 @@ $options = array(
 							'label' => esc_html__( 'Left', 'dw' )
 						),
 					),
+					'home_half_computer_1' => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Show on computer devices', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Show', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hidden-md visible-xs',
+							'label' => esc_html__( 'Hide', 'dw' )
+						),
+					),
+					'home_half_mobile_1'   => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Show on mobile devices', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Show', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hidden-xs',
+							'label' => esc_html__( 'Hide', 'dw' )
+						),
+					),
 				)
 			),
-			'full_width_2'  => array(
+			'full_width_2'   => array(
 				'title'   => esc_html__( 'Full Section 2', 'dw' ),
 				'type'    => 'tab',
 				'options' => array(
-					'home_full_width_2' => array(
+					'home_full_enable_2'   => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Activate', 'dw' ),
+						'desc'         => esc_html__( 'Show/Hide This section', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Yes', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hide',
+							'label' => esc_html__( 'No', 'dw' )
+						),
+					),
+					'home_full_width_2'    => array(
 						'type'            => 'addable-popup',
 						'label'           => esc_html__( 'Home Full Width Area 2', 'dw' ),
 						'template'        => '{{- block_title }}',
@@ -88,13 +182,53 @@ $options = array(
 							'layout_type' => dw_full_width_area()
 						)
 					),
+					'home_full_computer_2' => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Show on computer devices', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Show', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hidden-md visible-xs',
+							'label' => esc_html__( 'Hide', 'dw' )
+						),
+					),
+					'home_full_mobile_2'   => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Show on mobile devices', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Show', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hidden-xs',
+							'label' => esc_html__( 'Hide', 'dw' )
+						),
+					),
 				)
 			),
-			'half_section_2'  => array(
+			'half_section_2' => array(
 				'title'   => esc_html__( 'Half Section 2', 'dw' ),
 				'type'    => 'tab',
 				'options' => array(
-					'home_half_width_2' => array(
+					'home_half_enable_2'   => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Activate', 'dw' ),
+						'desc'         => esc_html__( 'Show/Hide This section', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Yes', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hide',
+							'label' => esc_html__( 'No', 'dw' )
+						),
+					),
+					'home_half_width_2'    => array(
 						'type'            => 'addable-popup',
 						'label'           => esc_html__( 'Home Half Width Area 2', 'dw' ),
 						'template'        => '{{- block_title }}',
@@ -111,7 +245,7 @@ $options = array(
 							'layout_type' => dw_half_width_area()
 						)
 					),
-					'sidebar_area_2'      => array(
+					'sidebar_area_2'       => array(
 						'type'         => 'switch',
 						'value'        => 'left_sidebar',
 						'label'        => esc_html__( 'Area (2) Sidebar Position', 'dw' ),
@@ -122,6 +256,32 @@ $options = array(
 						'left-choice'  => array(
 							'value' => 'left_sidebar',
 							'label' => esc_html__( 'Left', 'dw' )
+						),
+					),
+					'home_half_computer_2' => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Show on computer devices', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Show', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hidden-md visible-xs',
+							'label' => esc_html__( 'Hide', 'dw' )
+						),
+					),
+					'home_half_mobile_2'   => array(
+						'type'         => 'switch',
+						'value'        => 'show',
+						'label'        => esc_html__( 'Show on mobile devices', 'dw' ),
+						'right-choice' => array(
+							'value' => 'show',
+							'label' => esc_html__( 'Show', 'dw' )
+						),
+						'left-choice'  => array(
+							'value' => 'hidden-xs',
+							'label' => esc_html__( 'Hide', 'dw' )
 						),
 					),
 				)
--- wp-content/themes/infinity-mag/sidebar.php
+++ wp-content/themes/infinity-mag/sidebar.php
@@ -1,3 +1,3 @@
 <div class="sidebar" id="sidebar">
-	<?php dynamic_sidebar( 'home_side_bar' ); ?>
+	<?php dynamic_sidebar( 'first_side_bar' ); ?>
 </div>
